5213f16ddf99b4b47873eda2d9cee761533c7b70
Duwab <contact@superpaas.pro>
2020-02-01 16:59:04 +0100

    Add fontawesome

--- README.md
+++ README.md
@@ -25,3 +25,39 @@ Run `ng e2e` to execute the end-to-end tests via [Protractor](http://www.protrac
 ## Further help
 
 To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI README](https://github.com/angular/angular-cli/blob/master/README.md).
+
+## Icons
+
+### Font awesome 
+
+List : https://fontawesome.com/icons?d=gallery&m=free
+
+Doc : https://fontawesome.com/how-to-use/on-the-web/referencing-icons/basic-use
+
+Free version, use `fas` (solid style) or `fab` (brands style)
+```
+<i class="fas fa-camera"></i>
+```
+
+### Angular Material
+
+List : https://material.io/resources/icons/?style=baseline
+
+Import `MatIconModule` and use `mat-icon` directive :
+```
+<mat-icon>calendar_today</mat-icon>
+```
+
+### Octoicon
+
+List : https://octicons.github.com/
+
+Use directive configured in `theme/directives/octoicon.directive` :
+```
+<span octicon="gear"></span>
+```
+
+
+
+
+
--- src/app/shared/theme/directives/octoicon.directive.ts
+++ src/app/shared/theme/directives/octoicon.directive.ts
@@ -1,34 +1,56 @@
-import { Directive, Input, OnInit, ElementRef, Renderer2 } from '@angular/core';
+import { Directive, Input, OnInit, ElementRef, Renderer2, AfterViewInit } from '@angular/core';
 
 import * as octicons from '@primer/octicons';
 
 @Directive({
   selector: '[octicon]'
 })
-export class OcticonDirective implements OnInit {
+export class OcticonDirective implements OnInit, AfterViewInit {
 
   @Input() octicon: string;
   @Input() color: string;
   @Input() width: number;
 
-  constructor(private elementRef: ElementRef, private renderer: Renderer2) { }
+  constructor(private elementRef: ElementRef, private renderer: Renderer2) {
+  }
 
   ngOnInit(): void {
-    const el: HTMLElement = this.elementRef.nativeElement;
-    el.innerHTML = octicons[this.octicon].toSVG();
-console.log(window.getComputedStyle(this.elementRef.nativeElement));
-    const contextColor = window.getComputedStyle(this.elementRef.nativeElement).color;
+    const el: HTMLElement = this.getElement();
+    el.innerHTML = this.generateIcon();
 
-    console.log("contextColor", contextColor);
-
-    const icon: Node = el.firstChild;
-    if (this.color || contextColor) {
-      this.renderer.setStyle(icon, 'fill', this.color || contextColor);
+    if (this.color) {
+      this.applyStyle('fill', this.color);
     }
     if (this.width) {
-      this.renderer.setStyle(icon, 'width', this.width);
-      this.renderer.setStyle(icon, 'height', '100%');
+      this.applyStyle('width', this.width);
+      this.applyStyle('height', '100%');
     }
+  }
+
+  ngAfterViewInit(): void {
+    if (!this.color) {
+      const contextColor = window.getComputedStyle(this.elementRef.nativeElement).color;
+      this.applyStyle('fill', contextColor);
+    }
+  }
+
+  private applyStyle(styleName: string, value: any) {
+    const icon: Node|void = this.getIcon();
+    if (icon) {
+      this.renderer.setStyle(icon, styleName, value);
+    }
+  }
+
+  private getElement() {
+    return this.elementRef.nativeElement;
+  }
+
+  private getIcon(): Node|void {
+    return this.getElement().firstChild;
+  }
+
+  private generateIcon() {
+    return octicons[this.octicon].toSVG();
   }
 
 }
